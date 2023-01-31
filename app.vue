41a8c0236969aa3d94aff34aedb785079687ad5e
Markus Piotrowski <Markuspiotrowski@live.com>
2023-01-31 10:49:40 +0100

    Innit

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>
+      <header>
+        <h1>Logistiksystem</h1>
+        <nav class="navbar">
+          <nuxt-link to="/">Home</nuxt-link>
+        </nav>
+      </header>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+.navbar {
+  background-color: olivedrab;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+}
+.navbar * {
+  width: auto;
+  background-color: red;
+}
+</style>
