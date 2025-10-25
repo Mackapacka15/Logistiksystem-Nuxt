6de63a3d3a47f89aea5d12674616edc09c2ae4e7
Markus Piotrowski <markuspiotrowski@live.com>
2025-10-25 13:38:14 +0200

    Add dockerfile & update nuxt version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,4 @@ export default defineNuxtConfig({
     strict: true,
   },
   css: ["@/assets/css/global.css"],
-  runtimeConfig: {
-    DB_URL: process.env.MONGODB_URL || "localhost",
-    DB_Name: process.env.DB_Name || "logistiksystem",
-    DB_User: process.env.DB_User || "",
-    DB_Password: process.env.DB_Password || "",
-  },
 });
--- server/plugins/database.ts
+++ server/plugins/database.ts
@@ -1,17 +1,14 @@
 import mongoose from "mongoose";
 import autoIncrement from "mongoose-auto-increment";
 
-export default defineNitroPlugin(async (nitroApp) => {
-  const config = useRuntimeConfig();
-  const {
-    DB_URL,
-    DB_Name,
-    DB_User,
-    DB_Password,
-  }
-  = config;
+const DB_Host = process.env.DB_Host || "localhost";
+const DB_Name = process.env.DB_Name || "logistiksystem";
+const DB_User = process.env.DB_User || "";
+const DB_Password = process.env.DB_Password || "";
+
+export default defineNitroPlugin(async () => {
   const authPart = DB_User && DB_Password ? `${DB_User}:${DB_Password}@` : "";
-  const DB_URI = `mongodb://${authPart}${DB_URL}/${DB_Name}`;
+  const DB_URI = `mongodb://${authPart}${DB_Host}/${DB_Name}`;
   console.log("Connecting to DB at:", DB_URI);
   try {
     mongoose.set("strictQuery", true);
